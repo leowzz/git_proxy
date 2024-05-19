ff95d70a140e28cdc43d1f7af860916258a902a5
leo <3181538941@qq.com>
2024-05-19 21:57:45 +0800

    使用gitc 替代 git

--- gitc.py
+++ gitc.py
@@ -12,6 +12,9 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 default_conf = f"""[proxy]
 host = 127.0.0.1
 port = 7890
+
+[log]
+level = INFO
 """
 
 
@@ -47,6 +50,13 @@ class GitCloneProxy:
         else:
             logger.error(f"err_code{res.returncode}: {res.stderr}")
 
+    def execute_git_command(self):
+        git_command = "git " + " ".join(self.args.git_args)
+        logger.debug(f"<{git_command=}>")
+        # 输出到终端
+        res = subprocess.run(git_command, stdout=sys.stdout, stderr=sys.stderr, text=True, shell=True)
+        logger.debug(f"ret code<{res.returncode}>: {res.stderr}")
+
     def __enter__(self):
         self.set_proxy()
         return self
@@ -60,17 +70,9 @@ class GitCloneProxy:
 def init_cli():
     import argparse
     parser = argparse.ArgumentParser(description="gitc")
-    # 非必选参数
-    parser.add_argument("origin", help="git origin url", nargs="?", default="")
 
-    # 自定义参数
-    parser.add_argument("-s", "--set-proxy", action="store_true", help="set proxy")
-    parser.add_argument("-u", "--unset-proxy", action="store_true", help="unset proxy")
-    parser.add_argument("-d", "--debug", action="store_true", help="debug mode")
-    parser.add_argument("-V", "--version", action="version", version="1.0.0")
-
-    # 放行git clone 的参数
-    parser.add_argument('git_args', nargs=argparse.REMAINDER, help="git clone 原始参数")
+    # 放行所有git命令的参数
+    parser.add_argument('git_args', nargs=argparse.REMAINDER, help="git 原始参数")
 
     args = parser.parse_args()
     return args
@@ -150,31 +152,22 @@ def init_conf():
     config.read(conf_file)
     _host = config.get("proxy", "host")
     _port = config.getint("proxy", "port")
+    log_level = config.get("log", "level")
     logger.debug(f"从{conf_file}配置文件读取到 -> {_host=}, {_port=}")
-    return _host, _port
+    return _host, _port, log_level
 
 
 def main_cli():
     args = init_cli()
-    if not args.debug:
+    host, port, log_level = init_conf()
+    if "--debug" not in args.git_args:
         # 将日志等级设置为INFO
         logger.remove()
-        logger.add(sys.stdout, level="INFO")
-    host, port = init_conf()
+        logger.add(sys.stdout, level=log_level)
     logger.debug(f"{args=}")
-    if args.set_proxy:
-        GitCloneProxy(args, host, port).set_proxy()
-        return
-    elif args.unset_proxy:
-        GitCloneProxy(args, host, port).unset_proxy()
-        return
-    elif not args.origin:
-        logger.error("克隆地址不能为空")
-        # 按任意键退出
-        os.system("pause")
-        return
+
     with GitCloneProxy(args, host, port) as proxy:
-        proxy.clone()
+        proxy.execute_git_command()
 
 
 if __name__ == '__main__':
